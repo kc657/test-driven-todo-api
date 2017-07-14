ca1c44eb11966a7f6111385224b924cc8f9d093e
kc657 <kvn.0218@gmail.com>
2017-07-14 13:38:40 -0700

    Finished index and show

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,135 +1,131 @@
 // wait for DOM to load before running JS
-$(document).ready(function() {
-
+$(document).ready(function () {
   // base API route
-  var baseUrl = '/api/todos';
+  var baseUrl = '/api/todos'
 
   // array to hold todo data from API
-  var allTodos = [];
+  var allTodos = []
 
   // element to display list of todos
-  var $todosList = $('#todos-list');
+  var $todosList = $('#todos-list')
 
   // form to create new todo
-  var $createTodo = $('#create-todo');
+  var $createTodo = $('#create-todo')
 
   // compile handlebars template
-  var source = $('#todos-template').html();
-  var template = Handlebars.compile(source);
+  var source = $('#todos-template').html()
+  var template = Handlebars.compile(source)
 
   // helper function to render all todos to view
   // note: we empty and re-render the collection each time our todo data changes
-  function render() {
+  function render () {
     // empty existing todos from view
-    $todosList.empty();
+    $todosList.empty()
 
     // pass `allTodos` into the template function
-    var todosHtml = template({ todos: allTodos });
+    var todosHtml = template({ todos: allTodos })
 
     // append html to the view
-    $todosList.append(todosHtml);
+    $todosList.append(todosHtml)
   };
 
   // GET all todos on page load
   $.ajax({
-    method: "GET",
+    method: 'GET',
     url: baseUrl,
-    success: function onIndexSuccess(json) {
-      console.log(json);
-
+    success: function onIndexSuccess (json) {
+      console.log(json)
       // set `allTodos` to todo data (json.data) from API
-      allTodos = json.todos;
-
+      allTodos = json.todos
       // render all todos to view
-      render();
+      render()
     }
-  });
+  })
 
   // listen for submit even on form
   $createTodo.on('submit', function (event) {
-    event.preventDefault();
+    event.preventDefault()
 
     // serialze form data
-    var newTodo = $(this).serialize();
+    var newTodo = $(this).serialize()
 
     // POST request to create new todo
     $.ajax({
-      method: "POST",
+      method: 'POST',
       url: baseUrl,
       data: newTodo,
-      success: function onCreateSuccess(json) {
-        console.log(json);
+      success: function onCreateSuccess (json) {
+        console.log(json)
 
         // add new todo to `allTodos`
-        allTodos.push(json);
+        allTodos.push(json)
 
         // render all todos to view
-        render();
+        render()
       }
-    });
+    })
 
     // reset the form
-    $createTodo[0].reset();
-    $createTodo.find('input').first().focus();
-  });
+    $createTodo[0].reset()
+    $createTodo.find('input').first().focus()
+  })
 
   // add event-handlers to todos for updating/deleting
   $todosList
 
     // for update: submit event on `.update-todo` form
     .on('submit', '.update-todo', function (event) {
-      event.preventDefault();
+      event.preventDefault()
 
       // find the todo's id (stored in HTML as `data-id`)
-      var todoId = $(this).closest('.todo').attr('data-id');
+      var todoId = $(this).closest('.todo').attr('data-id')
 
       // find the todo to update by its id
       var todoToUpdate = allTodos.find(function (todo) {
-        return todo._id == todoId;
-      });
+        return todo._id == todoId
+      })
 
       // serialze form data
-      var updatedTodo = $(this).serialize();
+      var updatedTodo = $(this).serialize()
 
       // PUT request to update todo
       $.ajax({
         type: 'PUT',
         url: baseUrl + '/' + todoId,
         data: updatedTodo,
-        success: function onUpdateSuccess(json) {
+        success: function onUpdateSuccess (json) {
           // replace todo to update with newly updated version (json)
-          allTodos.splice(allTodos.indexOf(todoToUpdate), 1, json);
+          allTodos.splice(allTodos.indexOf(todoToUpdate), 1, json)
 
           // render all todos to view
-          render();
+          render()
         }
-      });
+      })
     })
 
     // for delete: click event on `.delete-todo` button
     .on('click', '.delete-todo', function (event) {
-      event.preventDefault();
+      event.preventDefault()
 
       // find the todo's id (stored in HTML as `data-id`)
-      var todoId = $(this).closest('.todo').attr('data-id');
+      var todoId = $(this).closest('.todo').attr('data-id')
 
       // find the todo to delete by its id
       var todoToDelete = allTodos.find(function (todo) {
-        return todo._id == todoId;
-      });
+        return todo._id == todoId
+      })
 
       // DELETE request to delete todo
       $.ajax({
         type: 'DELETE',
         url: baseUrl + '/' + todoId,
-        success: function onDeleteSuccess(json) {
+        success: function onDeleteSuccess (json) {
           // remove deleted todo from all todos
-          allTodos.splice(allTodos.indexOf(todoToDelete), 1);
+          allTodos.splice(allTodos.indexOf(todoToDelete), 1)
 
           // render all todos to view
-          render();
+          render()
         }
-      });
-    });
-
-});
+      })
+    })
+})
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // require express and other modules
 var express = require('express'),
-    app = express(),
-    bodyParser = require('body-parser');
+  app = express(),
+  bodyParser = require('body-parser')
 
 // configure bodyParser (for receiving form data)
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // serve static files from public folder
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'))
 
 /************
  * DATABASE *
@@ -15,10 +15,11 @@ app.use(express.static(__dirname + '/public'));
 
 // our database is an array for now with some hardcoded values
 var todos = [
-  { _id: 7, task: 'Laundry', description: 'Wash clothes' },
-  { _id: 27, task: 'Grocery Shopping', description: 'Buy dinner for this week' },
-  { _id: 44, task: 'Homework', description: 'Make this app super awesome!' }
+  { _id: 7, task: 'Laundry', description: 'Wash clothes'},
+  { _id: 27, task: 'Grocery Shopping', description: 'Buy dinner for this week'},
+  { _id: 44, task: 'Homework', description: 'Make this app super awesome!'}
 ];
+
 
 /**********
  * ROUTES *
@@ -28,10 +29,9 @@ var todos = [
  * HTML Endpoints
  */
 
-app.get('/', function homepage(req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/views/index.html')
+})
 
 /*
  * JSON API Endpoints
@@ -43,48 +43,49 @@ app.get('/', function homepage(req, res) {
  * ORDER THAT THE TESTS DICTATE.
  */
 
-app.get('/api/todos/search', function search(req, res) {
+app.get('/api/todos/search', function search (req, res) {
   /* This endpoint responds with the search results from the
    * query in the request. COMPLETE THIS ENDPOINT LAST.
    */
+})
+
+app.get('/api/todos', function index (req, res) {
+  res.json({data:todos})
+})
+
+app.post('/api/todos', function create (req, res) {
+  // var newTask = req.body.todos
+  // todos.push(newTask)
+})
+
+app.get('/api/todos/:id', function show (req, res) {
+  let idSelectedTask = parseInt(req.params.id) //parse 0 string into variable
+  let matchingTask = todos.filter(function (todo) {
+  return todo._id == idSelectedTask; //match parsed variable to our object id
+})[0];
+  // send foundTodo as JSON response
+  res.json(matchingTask);
 });
 
-app.get('/api/todos', function index(req, res) {
-  /* This endpoint responds with all of the todos
-   */
-});
-
-app.post('/api/todos', function create(req, res) {
-  /* This endpoint will add a todo to our "database"
-   * and respond with the newly created todo.
-   */
-});
-
-app.get('/api/todos/:id', function show(req, res) {
-  /* This endpoint will return a single todo with the
-   * id specified in the route parameter (:id)
-   */
-});
-
-app.put('/api/todos/:id', function update(req, res) {
+app.put('/api/todos/:id', function update (req, res) {
   /* This endpoint will update a single todo with the
    * id specified in the route parameter (:id) and respond
    * with the newly updated todo.
    */
-});
+})
 
-app.delete('/api/todos/:id', function destroy(req, res) {
+app.delete('/api/todos/:id', function destroy (req, res) {
   /* This endpoint will delete a single todo with the
    * id specified in the route parameter (:id) and respond
    * with success.
    */
-});
+})
 
 /**********
  * SERVER *
  **********/
 
 // listen on port 3000
-app.listen(3000, function() {
-  console.log('Server running on http://localhost:3000');
-});
+app.listen(3000, function () {
+  console.log('Server running on http://localhost:3000')
+})
